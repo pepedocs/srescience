f07a290cf8ed5f7981dfc78a946c08e4d289ba37
pepedocs <pepedocs@users.noreply.github.com>
2022-08-30 07:26:10 +0000

    deploy: 3d6a4434d7c6a370981afe5f3ffb7734462e18b2

---
+++ assets/documentation_service.md.613d8dee.lean.js
@@ -0,0 +1,1 @@
+import{_ as e,c as s,o as a,d as n}from"./app.411b39d2.js";const C=JSON.parse('{"title":"Service","description":"","frontmatter":{},"headers":[],"relativePath":"documentation/service.md"}'),t={name:"documentation/service.md"},i=n("",8),c=[i];function o(l,p,r,d,A,u){return a(),s("div",null,c)}const h=e(t,[["render",o]]);export{C as __pageData,h as default};

a00dd7c2fec9fad2af87003f31a268a988b9cda1
Jose Cueto <pepedocs@gmail.com>
2024-03-10 16:41:29 +1000

    added back head

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,5 +1,19 @@
 
 export default {
+  head: [
+    [
+      'script',
+      { async: '', src: 'https://www.googletagmanager.com/gtag/js?id=G-55B4X6Y17Y' }
+    ],
+    [
+      'script',
+      {},
+      `window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+      gtag('config', 'G-55B4X6Y17Y');`
+    ]
+  ],  
   title: 'srescience',
   description: 'Site-Reliability Engineering Science',
   themeConfig: {
